2681f5f3975fed57bab8348b61f7280b1efa59d6
Prashant Giri <amanprashantgiri>
2025-01-04 16:13:09 +0530

    fourth

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Specify the output directory
+    outDir: 'dist', // Ensure this points to 'dist'
   },
 })
